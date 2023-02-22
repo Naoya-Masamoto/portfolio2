05f2be5cc9728dc65587147bb88498b65d323b6f
Naoya-Masamoto <xenocrisis_kousaku@yahoo.co.jp>
2023-02-22 22:12:17 +0900

    sns_login_update

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -48,21 +48,4 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   #   super(scope)
   # end
 
-  private
-  
-  # コールバック時に行う処理
-  #def callback_from(provider)
-    #provider = provider.to_s
-
-    #@user = User.find_for_oauth(request.env['omniauth.auth'])
-
-    # persisted?でDBに保存済みかどうか判断
-    #if @user.persisted?
-      #flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: provider.capitalize)
-      #sign_in_and_redirect @user, event: :authentication
-    #else
-      #session["devise.#{provider}_data"] = request.env['omniauth.auth']
-      #redirect_to new_user_registration_url
-    #end
-  #end
 end
