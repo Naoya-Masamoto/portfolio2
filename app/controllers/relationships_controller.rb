c65461e60244486e7df832f8822ecdb7648f7a0b
Naoya-Masamoto <xenocrisis_kousaku@yahoo.co.jp>
2023-04-10 20:30:20 +0900

    relationships2

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,13 +1,26 @@
 class RelationshipsController < ApplicationController
   before_action :authenticate_user!
+  around_action :follow_user, only: %i[create]
+  around_action :unfollow_user, only: %i[destroy]
 
   def create
-    @user = User.find(params[:following_id])
     current_user.follow(@user)
   end
 
   def destroy
     @user = User.find(params[:id])
     current_user.unfollow(@user)
+  end
+
+  private
+
+  def follow_user
+    @user = User.find(params[:following_id])
+    yield
+  end
+
+  def unfollow_user
+    @user = User.find(params[:id])
+    yield
   end
 end
